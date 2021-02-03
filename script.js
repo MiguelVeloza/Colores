350dec764bac629d57967777123f108a80c09b6b
Miguel <mavelozao@correo.udistrital.edu.co>
2021-02-02 23:55:12 -0500

    cambios

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+function cambiar(){
+    var color = document.getElementById("caja").value;
+    var body = document.getElementById("body");
+    body.style.backgroundColor = color;
+    console.log(color);
+}
